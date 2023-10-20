b0948fc8fe62bc8b20ca671353521c4493b8bb59
mansi-jaimini <mansijaimini31@gmail.com>
2023-10-20 13:20:10 +0530

    todo application with class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component
+ {
+   state = {todo: []}
+
+   constructor(props)
+   {
+    super(props)
+     this.handleClick = this.handleClick.bind(this)
+   }
+  
+   handleClick(event)
+   {
+     event.preventDefault()
+    //console.log("handleClick is clicked")
+     let newTodo = document.getElementById("addTodo").value
+     let newTodoObject = {
+       id: new Date().getTime(),
+       text: newTodo,
+     }
+    console.log("Text readed: "+newTodo)
+    // console.log(newTodoObject)
+    // console.log(this.state)
+   // this.setState({counter:this.state.counter+1})
+    this.state.todo.push(newTodoObject)
+    this.setState({todo:this.state.todo})
+  // console.log(this.state)
+   document.getElementById("addTodo").value = ""
+   this.state.todo.map((tempTodo) =>{
+      return ("ID: "+tempTodo.id+", Text: "+tempTodo.text)
+    })
+    //alert("clicked")
+  } 
+   render()
+   {
+     return <div>
+       <h1>ToDo list</h1>
+       <form onSubmit={this.handleClick}>
+         <input type="text" id='addTodo'/>
+         <button type='submit'>Add To-do</button>
+       </form>
+       <div>
+        {this.state.todo.map((tempTodo) =>
+        {
+          return <div>{tempTodo.text}</div>
+         })         }
+      </div>
     </div>
-  );
+  }
 }
-
 export default App;
